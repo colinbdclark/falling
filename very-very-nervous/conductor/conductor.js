7017d578a4bdc3a52c0fb8f78e50205d3357911c
Colin Clark <colinbdclark@gmail.com>
2013-11-12 18:10:21 -0500

    Working conductor implementation.

--- very-very-nervous/conductor/conductor.js
+++ very-very-nervous/conductor/conductor.js
@@ -18,70 +18,101 @@
         
         invokers: {
             signal: {
-                funcName: "{that}.events.onSignal.fire",
-                args: ["{arguments}.0"]
+                funcName: "colin.conductor.signal",
+                args: ["{that}.options.messageMap", "{arguments}.0", "{leftClient}", "{rightClient}"]
             }
         },
         
-        component: {
+        components: {
             leftClient: {
                 type: "colin.conductor.client",
                 options: {
-                    host: "192.168.0.22"
+                    host: "192.168.0.22",
+                    listeners: {
+                        onError: "{conductor}.events.onError"
+                    }
                 }
             },
 
             rightClient: {
                 type: "colin.udpClient",
                 options: {
-                    host: "192.168.0.44"
+                    host: "192.168.0.44",
+                    listeners: {
+                        onError: "{conductor}.events.onError"
+                    }
                 }
             }
         },
         
         events: {
+            onSignal: null,
+            onError: null,
             onResponse: {
                 events: {
                     leftResponse: "{leftClient}.events.onResponse",
-                    rightResponse: "{rightClient}.events.onReponse",
-                    args: ["{arguments}.leftResponse.0", "{arguments}.rightResponse.0"]
-                }
+                    rightResponse: "{rightClient}.events.onResponse",
+                },
+                args: ["{arguments}.leftResponse.0", "{arguments}.rightResponse.0"]
             }
         },
         
         listeners: {
-            onResponse: {
-                "this": process,
-                method: "exit",
-                args: [0]
-            },
+            onResponse: [
+                {
+                    "this": console,
+                    method: "log",
+                    args: ["Recieved responses from all performers. ", "{arguments}.0", "{arguments}.1"]
+                },
+                {
+                    "this": process,
+                    method: "exit",
+                    args: [0]
+                }
+            ],
             
-            onError: {
-                "this": process,
-                method: "exit",
-                args: [1]
-            }
+            onError: [
+                {
+                    "this": console,
+                    method: "log",
+                    args: ["Recieved an error from a performer: ", "{arguments}.0"]
+                },
+                {
+                    "this": process,
+                    method: "exit",
+                    args: [1]
+                }
+            ]
         }
     });
+    
+    // TODO: This should be an event and registered declaratively.
+    colin.conductor.signal = function (messageMap, message, leftClient, rightClient) {
+        var val = messageMap[message];
+        
+        if (val === undefined) {
+            throw new Error("The specified message is unrecognized and could not be sent.");
+        }
+        
+        leftClient.sendFloat(val);
+        rightClient.sendFloat(val);
+    };
     
     fluid.defaults("colin.conductor.client", {
         gradeNames: ["colin.udpClient", "autoInit"],
 
         maxMessageLength: 4,
-        port: 65533,
-        
-        listeners: {
-            "{conductor}.events.onSignal": {
-                funcName: "{that}.sendFloat",
-                args: ["{arguments}.0"]
-            }
-        }
+        port: 65533
     });
     
     
     var message = process.argv[2],
         conductor = colin.conductor();
-        
-    conductor.signal(message);
+    
+    if (message) {
+        conductor.signal(message);
+    } else {
+        console.error("Please specify a message to send to the performer instances.");
+    }
 
 }());
