df6c10c851a4f623c140e086d98616416db67987
Colin Clark <colinbdclark@gmail.com>
2015-11-04 23:25:25 -0500

    Removes old motion response and synths in favour of asynchronous granular algorithm.

--- js/audio-players.js
+++ js/audio-players.js
@@ -1,4 +1,4 @@
-/*global fluid, flock, colin*/
+/*global fluid, flock*/
 
 (function () {
     "use strict";
@@ -28,23 +28,7 @@
             },
 
             motionTarget: {
-                type: "windvane.quadraphonicMotionTarget",
-                options: {
-                    components: {
-                        front: {
-                            options: {
-                                listeners: {
-                                    onMessage: [
-                                        {
-                                            funcName: "colin.falling.scaleMotionValue",
-                                            args: ["{band}", "{arguments}.1"]
-                                        }
-                                    ]
-                                }
-                            }
-                        }
-                    }
-                }
+                type: "windvane.quadraphonicMotionTarget"
             }
         }
     });
@@ -58,38 +42,4 @@
             }
         }
     });
-
-    var lastMove = Date.now();
-    colin.falling.scaleMotionValue = function (band, value) {
-        var scaled = value < 0.00005 ? 1.0 : 1.0 - (value * 5),
-            trigger = scaled > 0.5 ? 1.0 : 0.0,
-            isFairlyStill = value < 0.0005;
-
-        console.log("Raw value:", value, "scaled value:", scaled, "trigger value: ", trigger);
-
-        if (!isFairlyStill) {
-            lastMove = Date.now();
-        } else {
-            if (Date.now() - lastMove > 15000) {
-                // Sync right with left.
-                // TODO: This implementation is garbage, and produces it.
-                var synths = band.getSynths(),
-                    lastSynth = synths.pop(),
-                    idx = lastSynth.get("player").model.idx;
-
-                for (var i = 0; i < synths.length; i++) {
-                    synths[i].get("player").model.idx = idx;
-                }
-
-                lastMove = Date.now();
-                console.log("synchronizing");
-            }
-        }
-
-        band.set({
-            "player.speed": scaled,
-            "player.trigger": trigger,
-            "player.start": isFairlyStill ? 0 : Math.random()
-        });
-    };
 }());
--- js/synths.js
+++ js/synths.js
@@ -12,11 +12,100 @@
             expand: 1,
             sources: {
                 id: "player",
-                ugen: "flock.ugen.playBuffer",
-                loop: 1.0,
-                speed: 0.0,
-                start: 0.1,
-                trigger: 0.0
+                ugen: "flock.ugen.triggerGrains",
+                buffer: "{that}.options.buffer",
+                dur: {
+                    ugen: "flock.ugen.math",
+                    rate: "control",
+                    source: {
+                        ugen: "flock.ugen.math",
+                        rate: "control",
+                        source: 1.0,
+                        sub: {
+                            id: "grainDurScale",
+                            ugen: "flock.ugen.value",
+                            rate: "control",
+                            value: 0.0,
+                            mul: 5
+                        }
+                    },
+                    mul: {
+                        ugen: "flock.ugen.lfNoise",
+                        options: {
+                            interpolations: "linear"
+                        },
+                        freq: {
+                            id: "grainDurRandom",
+                            ugen: "flock.ugen.value",
+                            value: 0.0,
+                            add: 0.1
+                        },
+                        mul: 2.0,
+                        add: { //was 2.5
+                            id: "grainDurRandomAdd",
+                            ugen: "flock.ugen.value",
+                            value: 0.0,
+                            mul: 0.5,
+                            add: 5
+                        }
+                    }
+                },
+                speed: {
+                    ugen: "flock.ugen.math",
+                    source: 1.0,
+                    sub: {
+                        id: "speed",
+                        ugen: "flock.ugen.value",
+                        value: 0.0,
+                        mul: 0.2
+                    }
+                },
+                centerPos: {
+                    ugen: "flock.ugen.lfNoise",
+                    options: {
+                        interpolation: "linear"
+                    },
+                    freq: 10,
+                    mul: {
+                        ugen: "flock.ugen.bufferDuration",
+                        buffer: "{that}.options.buffer"
+                    }
+                },
+                trigger: {
+                    ugen: "flock.ugen.dust",
+                    density: {
+                        id: "grainDensity",
+                        ugen: "flock.ugen.value",
+                        rate: "control",
+                        value: 0.0,
+                        mul: 30,
+                        add: 0.1
+                    }
+                }
+            }
+        },
+
+        model: {
+            inputs: {
+                grainDensity: {
+                    value: "{motionTarget}.model.front.leftMotion"
+                },
+
+                grainDurScale: {
+                    value: "{motionTarget}.model.front.rightMotion"
+                },
+
+                grainDurRandom: {
+                    value: "{motionTarget}.model.front.leftMotion"
+                },
+
+                grainDurRandomAdd: {
+                    value: "{motionTarget}.model.front.rightMotion"
+                },
+
+                speed: {
+                    value: "{motionTarget}.model.front.rightMotion"
+                }
             }
         }
     });
@@ -24,10 +113,11 @@
     fluid.defaults("colin.falling.guitar", {
         gradeNames: "colin.falling.bufferSynth",
 
+        buffer: "guitar",
+
         synthDef: {
             bus: 0,
             sources: {
-                buffer: "guitar",
                 mul: 0.8
             }
         }
@@ -36,10 +126,20 @@
     fluid.defaults("colin.falling.piano", {
         gradeNames: "colin.falling.bufferSynth",
 
+        buffer: "piano",
+
         synthDef: {
-            bus: 1,
-            sources: {
-                buffer: "piano"
+            bus: 1
+        },
+
+        model: {
+            inputs: {
+                grainDensity: {
+                    value: "{motionTarget}.model.front.leftMotion"
+                },
+                grainDurScale: {
+                    value: "{motionTarget}.model.front.leftMotion"
+                }
             }
         }
     });
@@ -47,22 +147,20 @@
     fluid.defaults("colin.falling.uke", {
         gradeNames: "colin.falling.bufferSynth",
 
+        buffer: "uke",
+
         synthDef: {
-            bus: 2,
-            sources: {
-                buffer: "uke"
-            }
+            bus: 2
         }
     });
 
     fluid.defaults("colin.falling.vox", {
         gradeNames: "colin.falling.bufferSynth",
 
+        buffer: "vox",
+
         synthDef: {
-            bus: 3,
-            sources: {
-                buffer: "vox"
-            }
+            bus: 3
         }
     });
 
